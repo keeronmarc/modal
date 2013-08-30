b0eb0faad3284afd6629bfccc5c8c36ffea8df7f
keeronmarc <keeronmarc@gmail.com>
2013-08-30 10:33:14 -0400

    Add Modal

---
+++ modal.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function() {
+	$('.button1').click(function() {
+		console.log('what');
+		$('.modal').addClass('active');
+	})
+
+	$('.button2').click(function() {
+		// console.log('this is sooooo cool');
+		// 	window.getComputedStyle(modal.get(0)).getPropertyValue("top");
+		$('.modal').removeClass('active');
+	})
+	// var modal = $('.modal');
+	// console.log(getComputedStyle(modal.get(0)).changePropertyValue("top", "4px"));
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
